0a86e9fa6a949afd9e7fd9075d87508c0520aab9
ibraheemraw <ibraheemrawlinson@pursuit.org>
2019-09-21 21:02:06 -0400

    creating the section funxtion that fetches the abilities states moves ID from the enpoint

--- PokedexPro/Clients/PokeApiClient.swift
+++ PokedexPro/Clients/PokeApiClient.swift
@@ -28,8 +28,27 @@ struct PokeApiClient {
         task.resume()
     }
 
-    static func fetchPokemonEntryInfo(completionHandler handler: @escaping(Result<PokemonInfo,NetworkError>)-> Void){
-
+    static func fetchPokemonEntryInfo(pokemonUrl url: String, completionHandler handler: @escaping(Result<PokemonInfo,NetworkError>)-> Void){
+        let endpoint = url
+        guard let newUrl = URL(string: endpoint) else {
+            handler(.failure(.badURl))
+            return }
+        let request = URLRequest(url: newUrl)
+        let session = URLSession.shared
+        let task = session.dataTask(with: request) { (data, response, error) in
+            if let data = data {
+                do {
+                    let pokemonInfo = try JSONDecoder().decode(PokemonInfo.self, from: data)
+                    handler(.success(pokemonInfo))
+                } catch {
+                   handler(.failure(.jsonDecodeError(error)))
+                }
+            }
+            if let error = error {
+                handler(.failure(.apiError(error)))
+            }
+        }
+        task.resume()
     }
 
 }
