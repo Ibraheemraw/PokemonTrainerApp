4115722e18f553e981d3600f8f0dbf75d48563ef
ibraheemraw <ibraheemrawlinson@pursuit.org>
2019-09-21 20:34:58 -0400

    setting up the api cleints to fetch and decode the data

--- PokedexPro/Clients/PokeApiClient.swift
+++ PokedexPro/Clients/PokeApiClient.swift
@@ -2,4 +2,34 @@ import Foundation
 
 struct PokeApiClient {
     
+    private init(){}
+    //MARK: - Helper Methods
+    static func fetchPokemon(completionHandler handler: @escaping(Result<[Pokemon],NetworkError>)-> Void){
+        let endpoint = "https://pokeapi.co/api/v2/pokemon/"
+        guard let url = URL(string: endpoint) else {
+            handler(.failure(.badURl))
+            return
+        }
+        let request = URLRequest(url: url)
+        let session = URLSession.shared
+        let task = session.dataTask(with: request) { (data, response, error) in
+            if let data = data {
+                do {
+                    let result = try JSONDecoder().decode(Pokedex.self, from: data)
+                    handler(.success(result.results))
+                } catch {
+                    handler(.failure(.jsonDecodeError(error)))
+                }
+            }
+            if let error = error {
+                handler(.failure(.apiError(error)))
+            }
+        }
+        task.resume()
+    }
+
+    static func fetchPokemonEntryInfo(completionHandler handler: @escaping(Result<PokemonInfo,NetworkError>)-> Void){
+
+    }
+
 }
