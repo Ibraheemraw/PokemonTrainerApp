573d5aabafc4c7cbd2774ec33b99af04483f9ddd
ibraheemraw <ibraheemrawlinson@pursuit.org>
2019-09-22 22:45:48 -0400

    users is now able to add a edit their photo in the app.

--- PokedexPro/Controllers/EditTrainerProfileController.swift
+++ PokedexPro/Controllers/EditTrainerProfileController.swift
@@ -1,22 +1,67 @@
 import UIKit
+import Toucan
 
 class EditTrainerProfileController: UIViewController {
-
+    // MARK: - Outlets and Properties
+    @IBOutlet weak var profileImageView: CircularImageView!
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var regionField: UITextField!
+    @IBOutlet weak var numberOfBadgesLabel: UILabel!
+    @IBOutlet weak var bioTextView: UITextView!
+    private var imagePickerViewController: UIImagePickerController!
+    public var userProfileImage: UIImage?
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        callMethods()
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Actions and Methods
+    private func callMethods(){
+        setupOutlets()
     }
-    */
 
+    private func setupOutlets(){
+        usernameField.delegate = self
+        regionField.delegate = self
+        bioTextView.delegate = self
+    }
+    private func setupImagePicker(){
+        imagePickerViewController = UIImagePickerController()
+        imagePickerViewController.delegate = self
+        imagePickerViewController.allowsEditing = true
+        showImagePickerAlert(imagePickerVC: self.imagePickerViewController)
+        
+    }
+    private func showImagePicker(){
+        present(imagePickerViewController, animated: true, completion: nil)
+    }
+
+    @IBAction func changeProfileImage(_ sender: UIButton) {
+        setupImagePicker()
+        showImagePicker()
+    }
+    @IBAction func stepperDidPress(_ sender: UIStepper) {
+    }
+}
+// MARK: - Extensions
+extension EditTrainerProfileController: UITextFieldDelegate {}
+
+extension EditTrainerProfileController: UITextViewDelegate {}
+
+extension EditTrainerProfileController: UIImagePickerControllerDelegate,UINavigationControllerDelegate {
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let profileImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            self.profileImageView.contentMode = .scaleAspectFit
+            let resizedImage = Toucan(image: profileImage).resize(CGSize(width: 500, height: 500)).maskWithEllipse()
+            userProfileImage = resizedImage.image
+            self.profileImageView.image = userProfileImage
+        } else {
+            showAlert(alertTitle: "Error", alertMessage: "Orignal image is nil", alertStyle: .alert)
+        }
+         dismiss(animated: true, completion: nil)
+    }
 }
