d15ed399717e974fc5d2149d981f08f9f8375b5b
ibraheemraw <ibraheemrawlinson@pursuit.org>
2019-09-22 21:33:12 -0400

    added the files for each phone on the story baord and editng the UI for the Profile

--- PokedexPro/Controllers/MainController.swift
+++ PokedexPro/Controllers/MainController.swift
@@ -1,11 +1,3 @@
-//
-//  ViewController.swift
-//  PokedexPro
-//
-//  Created by Ibraheem rawlinson on 9/20/19.
-//  Copyright © 2019 Ibraheem rawlinson. All rights reserved.
-//
-
 import UIKit
 
 class MainController: UIViewController {
