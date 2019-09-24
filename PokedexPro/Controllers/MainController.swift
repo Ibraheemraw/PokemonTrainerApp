77d4370de355d48e5eba976f9e6672fe9025ad9a
ibraheemraw <ibraheemrawlinson@pursuit.org>
2019-09-23 23:06:02 -0400

    testing out the network call for the collection view

--- PokedexPro/Controllers/MainController.swift
+++ PokedexPro/Controllers/MainController.swift
@@ -1,13 +1,63 @@
 import UIKit
 
 class MainController: UIViewController {
-
+    // MARK: - Outlets and Properties
+    @IBOutlet weak var pokemonCollectionView: UICollectionView!
+    public var pokemon = [Pokemon](){
+        didSet{
+            DispatchQueue.main.async {
+                self.pokemonCollectionView.reloadData()
+                self.pokemonCollectionView.reloadInputViews()
+            }
+        }
+    }
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-       
-        
+        callMethods()
     }
-
+    // MARK: - Actions and Methods
+    private func callMethods(){
+        setupOutlets()
+       // fetchData()
+    }
+    private func setupOutlets(){
+        pokemonCollectionView.delegate = self
+        pokemonCollectionView.dataSource = self
+    }
+    private func fetchData(){
+        PokeApiClient.fetchPokemon { [weak self] (result) in
+            switch result {
+            case .success(let pokemon):
+                self?.pokemon = pokemon
+            case .failure(let error):
+                self?.showAlert(alertTitle: "Fetcing Data Error", alertMessage: "\(error)", alertStyle: .alert)
+            }
+        }
+    }
 
 }
 
+extension MainController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let pokemonCell = collectionView.dequeueReusableCell(withReuseIdentifier: "pokemonCell", for: indexPath) as? PokemonCell else {
+            showAlert(alertTitle: "Cell Creation Error", alertMessage: "was not able to load the custom cell created", alertStyle: .alert)
+            return UICollectionViewCell() }
+        //let settingCells = pokemon[indexPath.row]
+        //pokemonCell.pokemonName.text = settingCells.name
+        return pokemonCell
+    }
+}
+
+extension MainController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 0, height: 175)
+    }
+
+}
