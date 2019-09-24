a48808c89aab290c9411ed18f64b16d327b65caa
ibraheemraw <ibraheemrawlinson@pursuit.org>
2019-09-23 20:43:10 -0400

    created the file for the pokemon collection view

---
+++ PokedexPro/Cells/PokemonCell.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class PokemonCell: UICollectionViewCell {
+    // MARK: - Outlets and Properties
+    @IBOutlet weak var pokemonName: UILabel!
+    @IBOutlet weak var type1: UILabel!
+    @IBOutlet weak var type2: UILabel!
+    @IBOutlet weak var pokemonImg: UIImageView!
+    // MARK: - Life Cycle
+    // MARK: - Actions and Methods
+}
