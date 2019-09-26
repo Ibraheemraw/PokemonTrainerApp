4717ad5e9d6ed623a5c348f6cc83fd7d82d349f5
ibraheemraw <ibraheemrawlinson@pursuit.org>
2019-09-26 12:35:05 -0400

    added my generic ItemsDataManager to save delete and update

--- PokedexPro/SwiftyHelpers/ItemsDataManager.swift
+++ PokedexPro/SwiftyHelpers/ItemsDataManager.swift
@@ -1,38 +1,63 @@
-//import Foundation
-//
-//final class ItemsDataManager{
-//    private static let filenameOne = "MyPokedex.plist"
-//    private static let filenameTwo = "MyPokemonParty.plist"
-//
-//    static public func saveToDocumentsDirectory<T>(myModel model: T) -> (success: Bool, error: Error?){
-//        switch model {
-//        case is MyPokedex:
-//            let path = FileManagerHelper.filepathToDocumentsDirectory(filename: filenameOne)
-//            var myPokedex = fetchItemsFromDocumentsDirectory()
-//            myPokedex.append(model)
-//            do {
-//               let data = try PropertyListEncoder().encode(myPokedex)
-//                try data.write(to: path, options: Data.WritingOptions.atomic)
-//            } catch {
-//                print("property list decoding error: \(error)")
-//                return(false, nil)
-//            }
-//        case is MyPokemonParty:
-//            let path = FileManagerHelper.filepathToDocumentsDirectory(filename: filenameTwo)
-//            var myPokemonParty = fetchItemsFromDocumentsDirectory()
-//            myPokemonParty.append(model)
-//            do {
-//               let data = try PropertyListEncoder().encode(myPokemonParty)
-//                try data.write(to: path, options: Data.WritingOptions.atomic)
-//            } catch {
-//                print("property list decoding error: \(error)")
-//                return(false, nil)
-//            }
-//        default:
-//            print("no model chosen")
-//        }
-//         return(true, nil)
-//    }
-//    static public func fetchItemsFromDocumentsDirectory<T>() -> [T]{
-//    }
-//}
+import Foundation
+
+final class ItemsDataManager<T: Codable> where T: Equatable {
+  private  var filename = "Documents.plist"
+
+  public func saveItemToDocumentsDirectory(item: T) {
+    let path = FileManagerHelper.filepathToDocumentsDirectory(filename: filename)
+    var items = fetchItemsFromDocumentsDirectory()
+    items.append(item)
+    do {
+      let data = try PropertyListEncoder().encode(items)
+      try data.write(to: path, options: Data.WritingOptions.atomic)
+    } catch {
+      print("property list decoding error: \(error)")
+    }
+  }
+
+  public func fetchItemsFromDocumentsDirectory() -> [T] {
+    let path = FileManagerHelper.filepathToDocumentsDirectory(filename: filename).path
+    var items = [T]()
+    if FileManager.default.fileExists(atPath: path) {
+      if let data = FileManager.default.contents(atPath: path) {
+        do {
+          items = try PropertyListDecoder().decode([T].self, from: data)
+        } catch {
+          print("property list decoding error: \(error)")
+        }
+      } else {
+        print("data is nil")
+      }
+    } else {
+      print("\(filename) does not exist")
+    }
+    return items
+  }
+
+  public func updateItem(item: T, atIndex index: Int) {
+    let path = FileManagerHelper.filepathToDocumentsDirectory(filename: filename)
+    var items = fetchItemsFromDocumentsDirectory()
+    items[index] = item
+    do {
+      let data = try PropertyListEncoder().encode(items)
+      try data.write(to: path, options: Data.WritingOptions.atomic)
+    } catch {
+      print("property list decoding error: \(error)")
+    }
+  }
+
+  public func deleteItem(atIndex index: Int, item: T) {
+    let path = FileManagerHelper.filepathToDocumentsDirectory(filename: filename)
+    var items = fetchItemsFromDocumentsDirectory()
+    let index = items.firstIndex { $0 == item }
+    if let foundIndex = index {
+      items.remove(at: foundIndex)
+    }
+    do {
+      let data = try PropertyListEncoder().encode(items)
+      try data.write(to: path, options: Data.WritingOptions.atomic)
+    } catch {
+      print("property list decoding error: \(error)")
+    }
+  }
+}
